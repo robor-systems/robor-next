0314f9c09179675d924434326681faba5abde83e
M. Ahmed <62598421+mahmedtan@users.noreply.github.com>
2021-09-02 03:42:07 +0500

    Add static import to card

--- utils/seo.js
+++ utils/seo.js
@@ -1,4 +1,3 @@
-import cardImg from "public/assets/logos/robor-card-img.png";
 const seo = {
   title: "Robor",
   description:
@@ -10,7 +9,11 @@ const seo = {
     description:
       "We specialise at developing web applications that help you build and scale your business.",
 
-    images: [{ url: cardImg }],
+    images: [
+      {
+        url: "https://icecube-eu-304.icedrive.io/download?p=Uxl5TD8NFWR77iDafmwdGijEZamTvyA1LzGOY6XGLRef9fGGGM4DExJvy540pY9TITd6AjDuwawMJGEKrAFno2sICuLpqaPrPtcNiwr17oLZgr0%2BMhxUN%2FgcX7Tp6qdliwUvU4hQsgQjvCiBpdhyGXAH6MnYc3W%2FEp6847nqD95lNvEsp%2F1E61ZqJNxRXhISjtjgY3bYFkH1NVbA8hmnzQ%3D%3D",
+      },
+    ],
     site_name: "Robor",
     type: "website",
   },
