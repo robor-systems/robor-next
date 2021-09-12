9119e62aaf5ec9b9bfe8c65931ca0300a201626d
Danish <danishyasin33@gmail.com>
2021-09-12 21:36:28 +0500

    fix seo and remove redundant files

--- utils/seo.js
+++ utils/seo.js
@@ -1,17 +1,15 @@
 const seo = {
   title: "Robor",
-  description:
-    "We specialise at developing web applications that help you build and scale your business.",
+  description: "We ♥ building robust systems.",
   canonical: "https://robor.systems",
   openGraph: {
     url: "https://robor.systems",
     title: "Robor",
-    description:
-      "We specialise at developing web applications that help you build and scale your business.",
+    description: "We ♥ building robust systems.",
 
     images: [
       {
-        url: "https://icecube-eu-304.icedrive.io/download?p=Uxl5TD8NFWR77iDafmwdGijEZamTvyA1LzGOY6XGLRef9fGGGM4DExJvy540pY9TITd6AjDuwawMJGEKrAFno2sICuLpqaPrPtcNiwr17oLZgr0%2BMhxUN%2FgcX7Tp6qdliwUvU4hQsgQjvCiBpdhyGXAH6MnYc3W%2FEp6847nqD95lNvEsp%2F1E61ZqJNxRXhISjtjgY3bYFkH1NVbA8hmnzQ%3D%3D",
+        url: "https://robor-team.s3.eu-central-1.amazonaws.com/robor-card-img.png",
       },
     ],
     site_name: "Robor",
