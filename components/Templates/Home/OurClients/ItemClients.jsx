8ef58ef6a0413f32fc59a9d1de72c6c2b3ac8634
Ahmed Tan <mahmedexec@gmail.com>
2021-09-14 16:06:40 +0500

    Added grayscale effect

--- components/Templates/Home/OurClients/ItemClients.jsx
+++ components/Templates/Home/OurClients/ItemClients.jsx
@@ -16,7 +16,7 @@ const ItemClients = ({ visible, delay, ...props }) => {
       <a href={props.link} target="_blank" rel="noopener noreferrer">
         <ImageNext
           {...props}
-          className="w-28 h-12 transition-all duration-300 hover:brightness-50 brightness-90 sm:w-36 sm:h-16"
+          className="w-28 h-12 transition-all duration-300 grayscale hover:grayscale-0  sm:w-36 sm:h-16"
         />
       </a>
     </motion.div>
