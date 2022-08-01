d6eaaeb865adc27126b924ff9c5f9840ef8b9765
iqra fareed <iqrafareed56@gmail.com>
2022-08-01 21:09:25 +0500

    spacing of work page

--- components/Templates/Work/ImagesProject.jsx
+++ components/Templates/Work/ImagesProject.jsx
@@ -103,9 +103,9 @@ const ImagesProject = ({ images, imageCount, slug    ,slider}) => {
             variants={doubleImageVariant.topImage}
             transition={{ duration: 1.5 }}
             className={clsx(
-              "absolute md:left-0 right-[65px] z-10 overflow-hidden shadow-lg  lg:top-[400px] md:top-[240px]  top-[20px] ml-[20px]  md:ml-0",
+              "absolute md:left-0 right-[65px] z-10 overflow-hidden shadow-lg  lg:top-[400px]   top-[20px] ml-[20px]  md:ml-0",
               `double-image-${slug}`,
-              slider ? ' xl:right-[160px] lg:top-[250px] md:top-[350px] sm:top-[130px] lg:right-[94px] md:right-[75px]  sm:right-[150px]': "lg:top-[380px]"
+              slider ? ' xl:right-[160px] lg:top-[250px] md:top-[350px] sm:top-[130px] lg:right-[94px] md:right-[75px]  sm:right-[150px]': "lg:top-[380px] md:top-[330px] xl:right-[94]"
             )}
           
           >
@@ -128,10 +128,10 @@ const ImagesProject = ({ images, imageCount, slug    ,slider}) => {
             className={clsx(
               "absolute left-[80px] z-0 shadow-lg     mr-[10px] md:mr-0  ",
               `double-image-${slug}`,
-              slider ? 'xl:top-[2px] lg:top-[12px] md:top-[200px] sm:top-[10px] top-[-100px] xl:left-[114px] lg:left-[94px] ': "top-[-150px] lg:top-[90px] lg:left-[250px] sm:top-[100px] overflow-hidden "
+              slider ? 'xl:top-[2px] lg:top-[12px] md:top-[200px] sm:top-[10px] top-[-100px] xl:left-[114px] lg:left-[94px] ': " xl:top-[150px] lg:top-[90px] sm:top-[100px] top-[-150px] xl:left-[310px] lg:left-[140px]  overflow-hidden "
             )}
           >
-           <div className={!slider && (" relative  lg:top-[-270px] md:top-[-80px]")}>
+           <div className={!slider ? (" relative xl:top-[-170px] lg:top-[-90px] md:top-[-150px]"):""}>
             <Image
               src={images[1].imageUrl}
               width={images[1].width}
