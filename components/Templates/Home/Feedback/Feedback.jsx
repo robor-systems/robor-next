1b19a128f723eca5b71342ecf22e098c6e0913e9
iqra fareed <iqrafareed56@gmail.com>
2022-08-01 15:08:01 +0500

    spacing in home page

--- components/Templates/Home/Feedback/Feedback.jsx
+++ components/Templates/Home/Feedback/Feedback.jsx
@@ -22,7 +22,7 @@ const Feedback = () => {
             animate={visible ? "visible" : "invisible"}
             variants={opacityVariants}
             transition={{ duration: 1 }}
-            className="flex flex-col w-full h-full gap-12 pt-10 pb-12 lg:flex-row lg:gap-20 lg:py-16 lg:px-4"
+            className="flex flex-col w-full h-full gap-12 pt-10 pb-12 lg:flex-row lg:gap-20 lg:pb-16 lg:pt-52 lg:px-4"
           >
             <HeaderFeedback />
             <FormFeedback />
