2a88c47a3095f077ff89fa428816f143c4053765
iqra fareed <iqrafareed56@gmail.com>
2022-08-02 17:39:25 +0500

    remove box shadows from our process

--- components/Templates/Home/OurProcess/ItemProcess.jsx
+++ components/Templates/Home/OurProcess/ItemProcess.jsx
@@ -12,14 +12,7 @@ const ItemProcess = ({ visible, delay, ...props }) => {
       transition={{ duration: 1, delay }}
     >
       <motion.div
-        initial={{
-          boxShadow:
-            "6px 2px 16px 0px rgba(136, 165, 191, 0.3),   -6px -2px 16px 0px rgba(255, 255, 255, 1)",
-        }}
-        whileHover={{
-          boxShadow:
-            "3px 3px 6px 0px #ccdbe8 , -3px -3px 6px 1px rgba(255, 255, 255, 0.5)",
-        }}
+      
         transition={{ duration: 0.3 }}
         className="flex items-center justify-center p-4 bg-white rounded-2xl dark:bg-dark-bgPrimary"
       >
