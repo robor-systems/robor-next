c9fc7d2e0a7245e615acd0a07fa1682ea649383d
iqra fareed <iqrafareed56@gmail.com>
2022-09-01 11:27:36 +0500

    remove unused imports

--- components/Templates/CaseStudy/HeroCaseStudy/HeroCaseStudy.js
+++ components/Templates/CaseStudy/HeroCaseStudy/HeroCaseStudy.js
@@ -1,18 +1,7 @@
-import { ImageNext } from "@/components/Elements";
-import Image from "next/image";
-
 const HeroCaseStudy = ({ headerImage, title }) => {
   return (
-    <div className="relative ">
-      <div className=" h-[22vh] sm:h-[45vh] md:h-[45vh] lg:h-[380px] xl:h-[77vh] 3xl:h-[82vh]">
-        <Image
-          className="object-left"
-          src={headerImage}
-          alt={title}
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
+    <div>
+      <img src={headerImage} alt={title} layout="fill" objectFit="contain" />
     </div>
   );
 };
