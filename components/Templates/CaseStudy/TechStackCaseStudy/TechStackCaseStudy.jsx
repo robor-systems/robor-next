f570e2a28dbb361c0b504d11fef5eda557e6f742
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-13 00:28:46 +0500

    fix: removed useIsomorphicLayoutEffect + added transitions

--- components/Templates/CaseStudy/TechStackCaseStudy/TechStackCaseStudy.jsx
+++ components/Templates/CaseStudy/TechStackCaseStudy/TechStackCaseStudy.jsx
@@ -1,54 +1,20 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import clsx from "clsx";
 import SectionTechStack from "./SectionTechStack";
 import ReactVisibilitySensor from "react-visibility-sensor";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { useMediaQuery } from "react-responsive";
-import { useIsomorphicLayoutEffect } from "utils/hooks";
-gsap.registerPlugin(ScrollTrigger);
-const tl = gsap.timeline();
+import { motion } from "framer-motion";
+import {
+  leftTranslateVariant,
+  rightTranslateVariant,
+} from "constants/animations/variants.constant";
 
 const TechStackCaseStudy = ({ description, frontend, backend, other }) => {
-  const isBigScreen = useMediaQuery({ query: "(min-width: 768px)" });
   const [visible, setVisible] = useState(false);
-  const leftTextRef = useRef(null);
-  const rightTextRef = useRef(null);
 
-  useIsomorphicLayoutEffect(() => {
-    const el_Left = leftTextRef.current;
-    const el_Right = rightTextRef.current;
-    let rightVal = isBigScreen ? 150 : 20;
-    let leftVal = isBigScreen ? -150 : -20;
-    if (visible) {
-      tl.from(el_Left, { x: leftVal });
-      tl.to(el_Left, {
-        // scrollTrigger: {
-        //   trigger: el_Left,
-        //   start: "left right",
-        //   end: "right left",
-        // },
-
-        x: 0,
-      });
-
-      tl.from(el_Right, { x: rightVal });
-      tl.to(el_Right, {
-        // scrollTrigger: {
-        //   trigger: el_Right,
-        // },
-
-        x: 0,
-      });
-    }
-    return () => {
-      tl.kill();
-    };
-  }, [isBigScreen, visible]);
   return (
     <ReactVisibilitySensor
       partialVisibility
-      offset={{ bottom: 100 }}
+      offset={{ bottom: 200 }}
       onChange={(isVisible) => isVisible && setVisible(isVisible)}
     >
       <div
@@ -59,9 +25,12 @@ const TechStackCaseStudy = ({ description, frontend, backend, other }) => {
         )}
       >
         {/* * left content */}
-        <div
-          ref={leftTextRef}
+        <motion.div
           className="flex flex-col space-y-4 col-span-full md:col-span-1 text-light-contentSecondary dark:text-dark-contentSecondary"
+          initial="invisible"
+          variants={leftTranslateVariant}
+          animate={visible ? "visible" : "invisible"}
+          transition={{ duration: 1 }}
         >
           {/* * explore more heading */}
           <h4 className="project-info-heading ">Technology Stack</h4>
@@ -69,12 +38,15 @@ const TechStackCaseStudy = ({ description, frontend, backend, other }) => {
           <h1 className="pr-10 text-2xl font-bold sm:text-3xl md:text-4xl text-light-primary dark:text-dark-primary">
             Technological expertise provided by Robor
           </h1>
-        </div>
+        </motion.div>
         {/* * right content */}
 
-        <div
-          ref={rightTextRef}
+        <motion.div
           className="flex flex-col space-y-6 md:justify-end md:col-span-1 col-span-full"
+          initial="invisible"
+          variants={rightTranslateVariant}
+          animate={visible ? "visible" : "invisible"}
+          transition={{ duration: 1 }}
         >
           {/* * Description */}
           <p className=" text-md md:text-lg">{description}</p>
@@ -87,7 +59,7 @@ const TechStackCaseStudy = ({ description, frontend, backend, other }) => {
             {/* * Backend */}
             <SectionTechStack title="Backend" stack={backend} />
           </div>
-        </div>
+        </motion.div>
       </div>
     </ReactVisibilitySensor>
   );
