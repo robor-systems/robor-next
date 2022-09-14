682caae17510313481023374b31baf9eabbb56a4
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-14 22:25:31 +0500

    fix: fixed slider section responsiveness

--- components/Templates/Home/OurWork/SliderOurWork.jsx
+++ components/Templates/Home/OurWork/SliderOurWork.jsx
@@ -2,10 +2,8 @@ import Slider from "@/components/Modules/Carousel/Slider";
 import Container from "@/components/Modules/Container/Container";
 const SliderOurWork = () => {
   return (
-    <div className="container">
-      <div className="h-[480px] sm:h-[550px] max-w-5xl mx-auto">
-        <Slider />
-      </div>
+    <div className="h-[480px] sm:h-[550px]  mx-auto w-full max-w-xs sm:max-w-lg md:max-w-3xl xl:max-w-5xl 3xl:max-w-5xl">
+      <Slider />
     </div>
   );
 };
