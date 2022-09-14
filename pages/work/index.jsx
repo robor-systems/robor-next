acc655cef2db8136aa8eace8a8131cd5f218521d
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-14 22:21:44 +0500

    fix: fixed snackbar progress

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -1,19 +1,38 @@
 import Head from "next/head";
 import { ProjectWork } from "@/components/Templates/Work";
 import workProjects from "constants/content/work.constant";
+import { useEffect, useRef, useState } from "react";
+import { useElementScroll } from "framer-motion";
+import Snackbar from "@/components/Modules/Snackbar/Snackbar";
 
 const WorkPage = () => {
+  const ref = useRef(null);
+  const [scroll, setScroll] = useState(0);
+  const { scrollYProgress } = useElementScroll(ref);
+  useEffect(() => {
+    const handleScroll = (y) => {
+      setScroll(y * 100);
+    };
+    scrollYProgress.onChange(handleScroll);
+
+    return () => {
+      unsubscribeY();
+    };
+  });
   return (
     <>
       <Head>
         <title>Our Work | Robor Systems</title>
       </Head>
-
-      <main className="h-[100vh]  overflow-y-scroll snap snap-y snap-mandatory">
+      <main
+        ref={ref}
+        className="h-[100vh]  overflow-y-scroll snap snap-y snap-mandatory"
+      >
         {workProjects.map((project, index) => (
           <ProjectWork key={project.general.slug} {...project} index={index} />
         ))}
       </main>
+      <Snackbar scrollProgress={scroll} />
     </>
   );
 };
