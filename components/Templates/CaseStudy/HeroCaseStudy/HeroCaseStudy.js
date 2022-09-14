830bcdf58371d2314ad8172a6f5f912736b819f2
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-14 22:27:23 +0500

    fix: fixed height

--- components/Templates/CaseStudy/HeroCaseStudy/HeroCaseStudy.js
+++ components/Templates/CaseStudy/HeroCaseStudy/HeroCaseStudy.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const HeroCaseStudy = ({ headerImage, title }) => {
   return (
-    <div className=" h-[35vh] sm:h-[45vh] md:h-[60vh] lg:h-[380px] xl:h-[77vh] relative">
+    <div className=" h-[30vh] sm:h-[45vh] md:h-[60vh] lg:h-[380px] xl:h-[77vh] relative">
       <Image src={headerImage} alt={title} layout="fill" className="w-full" />
     </div>
   );
