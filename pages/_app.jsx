d6f0a1296500c2497c832a9cb3fcdbbc719f45d0
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-28 13:02:54 +0500

    feat: added google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,9 +3,28 @@ import { ThemeProvider } from "next-themes";
 import "styles/globals.css";
 
 import Layout from "@/components/Modules/Layout/Layout";
+import { pageview } from "utils/google-analytics";
 import seo from "utils/seo";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider attribute="class">
       <Layout>
