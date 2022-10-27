ca451212e33fac4b6c457ef16b645a39bb6565eb
Malaika Afridi <malaikaafridi22@gmail.com>
2022-10-27 23:21:09 +0500

    added solutions content

---
+++ constants/content/solutions.constant.js
@@ -0,0 +1,23 @@
+const solutions = [
+  {
+    general: {
+      slug: "image-detector",
+      title: "AI Image Generator",
+      description:
+        "Machine Learning application that detects objects from images. Duis a odio est. In pulvinar consequat lacus, in dictum turpis molestie et. Sed eu tortor, duis dui magna.",
+
+      workPageImageCount: 1,
+
+      workPageImages: [
+        {
+          imageUrl:
+            "https://robor-team.s3.eu-central-1.amazonaws.com/solutions/image-generator.png",
+          blurImageUrl:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAGCAMAAADNLv/0AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAtFBMVEV0d4NrbnliZXBZW2ZQU11JS1VDRU4+QEg6PEQ4OkFtcHtjZnFZXWdQUl1HSVM/Qko5O0MzNj0wMjktLzZmanVcYGpSVV9IS1Q+QUo3OUIwMzorLTQnKTAkJi1gZG9WWmRMT1lBRE04O0MvMjopLDIjJiwgIigdHyVbX2lRVV5GSlM8P0gyNT0qLTQeICYaHCIYGR9XW2VMUFpCRk43OkMtMDglKC8eIScZHCEVFx0TFRr///8HHqJ1AAAAAWJLR0Q7OQ70bAAAAAd0SU1FB+YKGwg7G3ml7WwAAAABb3JOVAHPoneaAAAASUlEQVQI1wXBAwKAQBAAwM22zct2/f9hzQBgOEFSNMNywAuiJCuqphtgWrbjen4QRhAnaZYXJapqaNquH0Y0zQus236c1/283w+WZgbVHKqLNgAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMi0xMC0yN1QwODo1OToyMCswMDowMPsKrx4AAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjItMTAtMjdUMDg6NTk6MjArMDA6MDCKVxeiAAAAAElFTkSuQmCC",
+        },
+      ],
+    },
+  },
+];
+
+export default solutions;
