2a12a3d2360da3d2f0318347b99c468334fe9ac6
Malaika Afridi <malaikaafridi22@gmail.com>
2022-11-02 16:05:47 +0500

    fix: rate-limit changes for production

--- pages/api/stable-diffusion.js
+++ pages/api/stable-diffusion.js
@@ -1,5 +1,32 @@
 import { generateAsync } from "stability-client";
-import { limiter, runMiddleware } from "utils/utils";
+import rateLimit from "express-rate-limit";
+// import { limiter, runMiddleware } from "utils/utils";
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+const getIP = (request) =>
+  request.ip ||
+  request.headers["x-forwarded-for"] ||
+  request.headers["x-real-ip"] ||
+  request.connection.remoteAddress;
+
+const limiter = rateLimit({
+  keyGenerator: getIP,
+  windowMs: 24 * 24 * 60 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  skipFailedRequests: true,
+  message: "We've reached our limit for now, please come back later",
+});
 
 export default async function handler(req, response) {
   const { prompt } = req.body;
