a4a0f2cf370dc3136fe71ec1acaa96b12aa6e595
Malaika Afridi <malaikaafridi22@gmail.com>
2022-11-02 12:06:05 +0500

    feat: apply rate limit for each IP

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,33 @@
+import rateLimit from "express-rate-limit";
+
 export const random = (min, max) => {
   return Math.random() * (max - min) + min;
 };
+
+export function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+const getIP = (request) =>
+  request.ip ||
+  request.headers["x-forwarded-for"] ||
+  request.headers["x-real-ip"] ||
+  request.connection.remoteAddress;
+
+const limit = 5;
+const windowMs = 24 * 24 * 60 * 60 * 1000;
+
+export const limiter = rateLimit({
+  keyGenerator: getIP,
+  windowMs,
+  max: limit,
+  skipFailedRequests: true,
+  message: "We've reached our limit for now, please come back later",
+});
