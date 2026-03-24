df6dcf35dc9709724026bbba2d08d02e18f67f8d
danish <danishyasin33@gmail.com>
2026-03-23 19:31:42 -0700

    fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { generateDefaultSeo } from "next-seo/pages";
 import { ThemeProvider } from "next-themes";
 import "styles/globals.css";
 
@@ -8,15 +7,15 @@ import Layout from "@/components/Modules/Layout/Layout";
 import { pageview } from "@/utils/google-analytics";
 import seo from "@/utils/seo";
 import { useRouter } from "next/router";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  const [seoData, setSeoData] = useState(seo);
+  const [canonical, setCanonical] = useState(seo.canonical);
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      setSeoData((prev) => ({...prev, canonical: "https://robor.systems" + url}));
+      setCanonical("https://robor.systems" + url);
       pageview(url);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
@@ -24,11 +23,23 @@ function MyApp({ Component, pageProps }) {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
+
   return (
     <ThemeProvider attribute="class">
       <ErrorBoundary>
         <Layout>
-          <Head>{generateDefaultSeo(seoData)}</Head>
+          <Head>
+            <title>{seo.title}</title>
+            <meta name="description" content={seo.description} />
+            <link rel="canonical" href={canonical} />
+            <meta property="og:url" content={seo.openGraph.url} />
+            <meta property="og:title" content={seo.openGraph.title} />
+            <meta property="og:description" content={seo.openGraph.description} />
+            <meta property="og:image" content={seo.openGraph.images[0].url} />
+            <meta property="og:site_name" content={seo.openGraph.site_name} />
+            <meta property="og:type" content={seo.openGraph.type} />
+            <meta name="twitter:card" content={seo.twitter.cardType} />
+          </Head>
           <Component {...pageProps} />
         </Layout>
       </ErrorBoundary>
